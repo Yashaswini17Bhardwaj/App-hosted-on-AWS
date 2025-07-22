b0b19e404a181c2122696cea03b00e105477245b
Temp <temp@Temps-MacBook-Pro.local>
2025-07-22 15:10:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
+const client = require('prom-client');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World from Demo App!'); // Fixed response text to match test
+// Enable metrics collection
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+const httpRequestDuration = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 10],
+});
+register.registerMetric(httpRequestDuration);
+
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'code'],
+});
+register.registerMetric(httpRequestCounter);
+
+// Middleware for metrics
+app.use((req, res, next) => {
+  const end = httpRequestDuration.startTimer();
+  res.on('finish', () => {
+    httpRequestCounter.inc({ method: req.method, route: req.path, code: res.statusCode });
+    end({ method: req.method, route: req.path, code: res.statusCode });
+  });
+  next();
 });
 
-module.exports = app; // Export the app for testing
+// Expose metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.setHeader('Content-Type', register.contentType);
+  res.send(await register.metrics());
+});
+
+// Your existing routes
+app.get('/', (req, res) => {
+  res.send('Hello World from Demo App!');
+});
+
+module.exports = app;
